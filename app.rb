a2fab2dd3f53d13e15e2f547c7d0a2f54717c98d
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-02-06 19:21:31 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -6,7 +6,6 @@
 p [1,2,3]
 puts "Hello World!"
 print "Pass this test, please."
- p [1,2,3]
 
 # def add
     
@@ -14,10 +13,10 @@ print "Pass this test, please."
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
